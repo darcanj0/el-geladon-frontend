4d8f5f7d17a8bc1b05a5d36d34afb4aca7aa9bb0
Daniel Arcanjo <daniel.thomas.aarcanjo@gmail.com>
2022-05-27 00:06:20 -0300

    refactor: class in script, create function name

--- assets/script.js
+++ assets/script.js
@@ -1,30 +1,114 @@
+//Requisitions dealing with CRUD operations
+class Requisitions {
+  async fetchAllPaletasAndUpdateArray() {
+    const response = await fetch(`${baseURL}/find-paletas`);
+
+    const paletas = await response.json();
+
+    if (paletas.message != undefined) {
+      //tratar erro de não achar nenhuma paleta no db(mensagem vem da api)
+      showAlertSpan(paletas.message, "Danger");
+    }
+
+    arrayPaletas = paletas;
+
+    return paletas;
+  }
+
+  async findPaletaById(id) {
+    const response = await fetch(`${baseURL}/find-paleta/${id}`);
+    const paleta = await response.json();
+    return paleta;
+  }
+
+  async createOrUpdatePaleta() {
+    const id = document.querySelector("#id").value;
+    const sabor = document.querySelector("#sabor").value;
+    const preco = document.querySelector("#preco").value;
+    const foto = document.querySelector("#foto").value;
+    const descricao = document.querySelector("#descricao").value;
+
+    const paleta = {
+      id,
+      sabor,
+      descricao,
+      foto,
+      preco,
+    };
+
+    const modoEdicao = id != "";
+
+    const endpoint =
+      baseURL + (modoEdicao ? `/update-paleta/${id}` : `/create-paleta`);
+
+    const response = await fetch(endpoint, {
+      method: modoEdicao ? "put" : "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      mode: "cors",
+      body: JSON.stringify(paleta),
+    });
+
+    const novaPaleta = await response.json();
+
+    if (novaPaleta.message != undefined) {
+      //tratar erro de mandar campos vazios tanto em cadastro quanto em atualização(mensagem vem da api)
+      showAlertSpan(novaPaleta.message, "Danger");
+    } else {
+      let message = "Paleta cadastrada com sucesso!";
+      // mostrar mensagem de cadastro/atualização com sucesso(mensagem nao vem da api)
+      if (modoEdicao) {
+        message = "Paleta atualizada com sucesso";
+      }
+      showAlertSpan(message, "Success");
+    }
+
+    const chosenPaletaDiv = document.getElementById("chosenPaleta");
+    chosenPaletaDiv.innerHTML = "";
+
+    printAllPaletas();
+    fecharModal();
+  }
+
+  async deletePaleta(id) {
+    // console.log(`Vou tentar deletar a de id ${id}`);
+    const response = await fetch(`${baseURL}/delete-paleta/${id}`, {
+      method: "delete",
+      mode: "cors",
+    });
+    const result = await response.json();
+
+    if (result.message != undefined) {
+      //tratar erro de n encontrar a paleta para deletar (mensagem vem da api)
+      showAlertSpan(result.message, "Danger");
+    } else {
+      //mostrar mensagem de deleção com sucesso (mensagem nao vem da api)
+      showAlertSpan("Paleta deletada com sucesso!", "Success");
+    }
+
+    const chosenPaletaDiv = document.getElementById("chosenPaleta");
+    chosenPaletaDiv.innerHTML = "";
+    closeDeleteModal();
+    printAllPaletas();
+  }
+}
+
+//Auxiliary variables
 // const baseURL = "https://el-geladon-api-v1.onrender.com/paletas";
 const baseURL = "http://localhost:3001/paletas";
 let arrayPaletas = [];
+const requisitions = new Requisitions();
 
-const fetchAllPaletasAndUpdateArray = async () => {
-  const response = await fetch(`${baseURL}/find-paletas`);
-
-  const paletas = await response.json();
-
-  if (paletas.message != undefined) {
-    //tratar erro de não achar nenhuma paleta no db(mensagem vem da api)
-    showAlertSpan(paletas.message, "Danger");
-  }
-
-  arrayPaletas = paletas;
-
-  return paletas;
-};
-
+//HTML manipulation functions
 const printAllPaletas = async () => {
-  const paletas = await fetchAllPaletasAndUpdateArray();
+  await requisitions.fetchAllPaletasAndUpdateArray();
 
   document.getElementById("avaliablePaletas").style.display = "flex";
   document.getElementById("searchResult").style.display = "none";
   document.querySelector("#paletaList").innerHTML = "";
 
-  paletas.forEach(function (paleta) {
+  arrayPaletas.forEach(function (paleta) {
     document.querySelector("#paletaList").insertAdjacentHTML(
       "beforeend",
       `
@@ -70,7 +154,7 @@ const findPaletaByName = async () => {
     return;
   }
 
-  const queriedPaleta = await findPaletaById(selectedPaleta._id);
+  const queriedPaleta = await requisitions.findPaletaById(selectedPaleta._id);
 
   if (queriedPaleta.message != undefined) {
     //tratar erro de não encontrar a paleta no db(mensagem vem da api);
@@ -81,13 +165,7 @@ const findPaletaByName = async () => {
   }
 };
 
-const findPaletaById = async (id) => {
-  const response = await fetch(`${baseURL}/find-paleta/${id}`);
-  const paleta = await response.json();
-  return paleta;
-};
-
-const printPaletaSearch = async (paleta) => {
+const printPaletaSearch = (paleta) => {
   const chosenPaletaDiv = document.getElementById("chosenPaleta");
 
   document.querySelector(".PaletaList").innerHTML = "";
@@ -139,8 +217,11 @@ const openModal = async (id = "") => {
       "Atualizar uma Paleta";
     document.querySelector("#submitModalButton").innerText = "Atualizar";
 
-    const response = await fetch(`${baseURL}/find-paleta/${id}`);
-    const paleta = await response.json();
+    const paleta = await requisitions.findPaletaById(id);
+
+    if (paleta.message != undefined) {
+      return showAlertSpan(paleta.message, "Danger");
+    }
 
     document.querySelector("#sabor").value = paleta.sabor;
     document.querySelector("#preco").value = paleta.preco;
@@ -165,56 +246,6 @@ const fecharModal = () => {
   document.querySelector("#id").value = "";
 };
 
-const createPaleta = async () => {
-  const id = document.querySelector("#id").value;
-  const sabor = document.querySelector("#sabor").value;
-  const preco = document.querySelector("#preco").value;
-  const foto = document.querySelector("#foto").value;
-  const descricao = document.querySelector("#descricao").value;
-
-  const paleta = {
-    id,
-    sabor,
-    descricao,
-    foto,
-    preco,
-  };
-
-  const modoEdicao = id != "";
-
-  const endpoint =
-    baseURL + (modoEdicao ? `/update-paleta/${id}` : `/create-paleta`);
-
-  const response = await fetch(endpoint, {
-    method: modoEdicao ? "put" : "post",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    mode: "cors",
-    body: JSON.stringify(paleta),
-  });
-
-  const novaPaleta = await response.json();
-
-  if (novaPaleta.message != undefined) {
-    //tratar erro de mandar campos vazios tanto em cadastro quanto em atualização(mensagem vem da api)
-    showAlertSpan(novaPaleta.message, "Danger");
-  } else {
-    let message = "Paleta cadastrada com sucesso!";
-    // mostrar mensagem de cadastro/atualização com sucesso(mensagem nao vem da api)
-    if (modoEdicao) {
-      message = "Paleta atualizada com sucesso";
-    }
-    showAlertSpan(message, "Success");
-  }
-
-  const chosenPaletaDiv = document.getElementById("chosenPaleta");
-  chosenPaletaDiv.innerHTML = "";
-
-  printAllPaletas();
-  fecharModal();
-};
-
 const openDeleteModal = (id) => {
   // console.log(`Abri o modal delete com o id ${id}`);
   document.querySelector("#overlayDelete").style.display = "flex";
@@ -224,7 +255,7 @@ const openDeleteModal = (id) => {
     `
 
   <button class="BtnDeleteNo BtnDelete" onclick="closeDeleteModal()">Não</button>
-  <button class="BtnDeleteYes BtnDelete" onclick="deletePaleta('${id}')">Sim</button>
+  <button class="BtnDeleteYes BtnDelete" onclick="requisitions.deletePaleta('${id}')">Sim</button>
 
   `
   );
@@ -233,28 +264,6 @@ const openDeleteModal = (id) => {
 const closeDeleteModal = () => {
   document.querySelector("#overlayDelete").style.display = "none";
   document.querySelector(".BtnsDelete").innerHTML = "";
-};
-
-const deletePaleta = async (id) => {
-  // console.log(`Vou tentar deletar a de id ${id}`);
-  const response = await fetch(`${baseURL}/delete-paleta/${id}`, {
-    method: "delete",
-    mode: "cors",
-  });
-  const result = await response.json();
-
-  if (result.message != undefined) {
-    //tratar erro de n encontrar a paleta para deletar (mensagem vem da api)
-    showAlertSpan(result.message, "Danger");
-  } else {
-    //mostrar mensagem de deleção com sucesso (mensagem nao vem da api)
-    showAlertSpan("Paleta deletada com sucesso!", "Success");
-  }
-
-  const chosenPaletaDiv = document.getElementById("chosenPaleta");
-  chosenPaletaDiv.innerHTML = "";
-  closeDeleteModal();
-  printAllPaletas();
 };
 
 const showAlertSpan = (message, type) => {
